c65a73d5d23cc12b1f60d3ff366331b3e715ef05
Max <mj31508@gmail.com>
2016-10-18 21:42:41 -0700

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+int main(void)
+{
+  int n = 1;
+  while (n > 0);
+  {
+  _putchar('H');
+  _putchar('o');
+  _putchar('l');
+  _putchar('b');
+  _putchar('e');
+  _putchar('r');
+  _putchar('t');
+  _putchar('o');
+  _putchar('n');
+  _putchar('\n');
+  n--;
+
+}
+  return 0;
+}
