a3ebdc0b35e62de5ebde608ce90af99380a1982a
Max <mj31508@gmail.com>
2016-10-26 16:58:56 -0700

    hey

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- *void reset_to_98(int *n) - using pointers
- *@n: The address of the value needed to change
+ * reset_to_98 - using pointers
+ * @n: The address of the value needed to change
  * Return: nothing
  */
 /**this is my function prototype*/
