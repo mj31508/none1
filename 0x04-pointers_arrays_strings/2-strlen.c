bfff969b500c712f64eccf57890f9a0e16f88227
Max <mj31508@gmail.com>
2016-10-27 09:55:47 -0700

    fix

--- 0x04-pointers_arrays_strings/2-strlen.c
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -4,11 +4,14 @@
 /**
  * _strlen - calculates the length of a string.
  *@s:pointer to the array character.
+ *
  * Return: Returning i.
  */
+
 int _strlen(char *s)
 {
 	int i;
+
 	i = 0;
 	while (*s != '\0')
 	{
