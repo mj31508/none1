d285d7a1617fa8626f49a1f5ce514ebbd263a70b
Max <mj31508@gmail.com>
2016-10-29 13:47:37 -0700

    added headers

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,20 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * _strcmp - Compare strings, return value difference
+ * @s1: first string to compare
+ * @s2: second string to compare
  *
- * Return: Always 0.
+ * Return: Value difference of strings
  */
-int main(void)
-{
-	char s1[] = "Hello";
-	char s2[] = "HELLO";
 
-	printf("%d\n", _strcmp(s1, s2));
-	printf("%d\n", _strcmp(s2, s1));
-	printf("%d\n", _strcmp(s1, s1));
-	return (0);
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i) != '\0' && *(s2 + i) != '\0')
+		i++;
+
+	return (*(s1 + i) - *(s2 + i));
 }
