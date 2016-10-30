ecf0529da9bad4c823317bd2b71647040c13f4ac
Max <mj31508@gmail.com>
2016-10-30 14:50:00 -0700

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * times_table - multiplication grid
+ * Return: n/a
+ */
+void times_table(void)
+{
+	int x, y;
+
+	x = 0;
+	while (x <= 9)
+	{
+		y = 0;
+		while (y <= 9)
+		{
+			if ((x * y) < 10)
+			{
+				if (y != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar((x * y) + '0');
+				if (y != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((x * y) / 10 + '0');
+				_putchar((x * y) % 10 + '0');
+				if (y != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
