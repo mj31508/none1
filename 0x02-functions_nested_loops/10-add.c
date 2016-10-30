d2585bd82035cde223e7adabc8ca271828f4c5df
Max <mj31508@gmail.com>
2016-10-30 14:24:09 -0700

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
-#include <stdio.h.
+#include <stdio.h>
+
 /**
  * add - adds to number together
  * @x: 1st int
  * @y: 2nd int
  * Return: num
  */
-void int( int i, int b)
+int add( int i, int b)
 {
 	return (x + y);
 }
