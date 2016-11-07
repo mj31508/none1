b4d0323cd9ae034b8cab3dd0ddcfaf57de71e7b2
Max <mj31508@gmail.com>
2016-11-06 21:09:34 -0800

    fix

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(9);
+	_putchar('\n');
+	print_times_table(12);
+	_putchar('\n');
+	return (0);
+}
