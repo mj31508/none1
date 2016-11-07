dc3c37a3aeb9398550a9ce5b14276a4b284d1e28
Max <mj31508@gmail.com>
2016-11-06 21:36:40 -0800

    fix

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -1,32 +1,18 @@
-#include "holberton.h"
-
 /**
- * *_strncpy - copies a string.
- * @dest: string to be appended
- * @src: string to be appended from
- * @n: number of bytes from src
- * Return: a pointer to the resulting string dest
- **/
+ * _strncpy - copies a string up to n
+ * @dest: Destination of the pointer to the string
+ * @src: Source of the pointer to the string to copy
+ * @n: Number of bytes to copy.
+ * Return: Pointer to the destination string.
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+int i;
 
-	if (n <= 0)
-	{
-		return (dest);
-	}
-
-	i = 0;
-
-	while (i < n && src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	while (i < n)
-	{
-		dest[i++] = '\0';
-	}
-	return (dest);
+for (i = 0; src[i] != '\0' && i < n; i++)
+	dest[i]= src[i];
+while (i < n)
+	dest[i++] = '\0';
+return (dest);
 }
