ed89f417c94a05f38fbd5729920d11882b56d12c
mj31508 <max.johnson@holbertonschool.com>
2016-11-09 01:08:56 +0000

    1

---
+++ 0x0A-malloc_free/1-main.c
@@ -0,0 +1,19 @@
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s;
+
+	s = _strdup("Holberton");
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
+}
