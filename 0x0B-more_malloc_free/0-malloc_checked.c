43e2ead9d737804f9ffdfa10bfaddf6ceb07889b
mj31508 <max.johnson@holbertonschool.com>
2016-11-11 21:18:46 +0000

    zero

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,10 @@
+#include "holberton.h"
 #include <stdlib.h>
 
 /**
  * malloc_checked - function for the unsigned int
  *
- *@b: interger argument
+ * @b: interger argument
  * Return: return a pointer to allocated memory
  */
 void *malloc_checked(unsigned int b)
