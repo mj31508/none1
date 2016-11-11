6cdf5c783c6d1b34d836e11313160a1985032d62
mj31508 <max.johnson@holbertonschool.com>
2016-11-11 21:02:03 +0000

    question 0

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function for the unsigned int
+ *
+ *@b: interger argument
+ * Return: return a pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *sampptr;
+
+	sampptr = malloc(b);
+
+	if(sampptr == NULL)
+		 exit(98);
+	else
+		return (sampptr);
+
+}
