573e11dc840974fe13f544b5f60fcecbab6f3cbe
mj31508 <max.johnson@holbertonschool.com>
2016-11-15 18:23:05 +0000

    first commit

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -8,9 +8,5 @@ struct dog {
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-int _putchar(char c);
+
 #endif // DOG
