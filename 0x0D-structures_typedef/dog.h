9d739357e7774ee6cab4a745ced59a2d65429aa0
mj31508 <max.johnson@holbertonschool.com>
2016-11-15 18:19:05 +0000

    first commit

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,12 +1,12 @@
 #ifndef DOG
 #define DOG
 
-typedef struct dog {
+struct dog {
 
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
