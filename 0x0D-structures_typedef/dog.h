a920b12d1d006d37de055b873ccba5c48737311e
mj31508 <max.johnson@holbertonschool.com>
2016-11-15 18:29:42 +0000

    first commit

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,12 @@
-#ifndef DOG
-#define DOG
-
+#ifndef DOGGY
+#define DOGGY
+/**
+* main - define a new type of struch dog
+* @char: pointer for name
+* @age: floater
+* @char: pointer for owner
+* Return: Always zero
+/**
 struct dog {
 
 	char *name;
