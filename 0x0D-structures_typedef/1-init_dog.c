0d232485c06e2f504169d60e828f65efc83b0947
mj31508 <max.johnson@holbertonschool.com>
2016-11-16 01:40:45 +0000

    dog

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -3,6 +3,7 @@
 
 /**
  * init_dog - initializing a variable type
+ * @d: pointer for dog
  * @age: pointer
  * @name: another pointer
  * @owner: for age variable
@@ -10,15 +11,7 @@
  **/
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
-/**
- * init_dog - takeps pointer to dog and initializes components
- *
- * @d: pointer to dog variable
- * @name: pointer of name to the dog
- * @age: float age of dog
- * @owner: pointer character of owner
- * Return: none
- **/
+
 {
 	if (d != NULL)
 	{
