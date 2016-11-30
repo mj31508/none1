43f02f1ac33c287745c83e92cfa702bb4cd7d849
mj31508 <max.johnson@holbertonschool.com>
2016-11-30 07:09:34 +0000

    3

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -34,7 +34,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		*head = new_node;
 		return (*head);
 	}
-	while (temp->next !=NULL)
+	while (temp->next != NULL)
 	{
 		temp = temp->next;
 	}
