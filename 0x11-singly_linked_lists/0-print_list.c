8135b9b4fbb569c5ff9ea6379f612c3f0e961d4a
mj31508 <max.johnson@holbertonschool.com>
2016-11-30 05:42:22 +0000

    again

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "list.h"
+#include <stdlib.h>
+
+/**
+ * print_list - prints out all of the elements of the list_t list
+ *@h: the head of the linked list
+ *Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t print;
+
+	print = 0;
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		print++;
+	}
+	return (count);
+}
