a33d4213bcf845546992f14d518751b8a3b72469
mj31508 <max.johnson@holbertonschool.com>
2016-12-01 19:50:43 +0000

    not finish yet

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at -inserts a new node at position
+ *@index: unsigned int
+ *@n: int
+ *@head: double pointer
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
+{
+	listint_t *temp, *temp2;
+	int n;
+}
