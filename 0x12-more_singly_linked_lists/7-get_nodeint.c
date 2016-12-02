d124ff57bbb44792d2281cd88adf5ff73bc9afcf
Max <mj31508@gmail.com>
2016-12-02 13:33:57 -0800

    7

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -14,12 +14,17 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int counter;
 
+	if (head == NULL)
+		return (NULL);
+
 	counter = 0;
-	while (counter <  index)
+	while (counter <  index && head != NULL)
 	{
 
 		head = head->next;
 		counter++;
 	}
+	if (counter < index)
+		return (NULL);
 	return (head);
 }
