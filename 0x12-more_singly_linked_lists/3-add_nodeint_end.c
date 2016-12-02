7c90e793ae0e2146c130e4466a29200a98563edd
Max <mj31508@gmail.com>
2016-12-01 16:29:35 -0800

    3

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,22 +14,27 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new, *temp;
 
-	temp = malloc(sizeof(listint_t);
-		      if (n == NULL)
-			      return (NULL);
-		      temp->n = n;
-		      temp->next = NULL;
-		      temp = *head;
+	temp = malloc(sizeof(listint_t));
 
-		      if (*head == NULL)
-		      {
-			      *head = new;
-			      return (*head);
-		      }
+	if (temp==NULL)
+	{
+		return (NULL);
+	}
 
-		      while (new->next != null)
-		      { temp = temp->next;
-			temp->next = new;
-		      }
-		      return (temp->next);
+	temp->n = n;
+	temp->next = NULL;
+
+	if (*head == NULL)
+
+	{
+		*head = temp;
+		return (*head);
+	}
+
+	while (temp->next!=NULL)
+	{
+		temp = temp->next;
+		temp->next = *head;
+	}
+	return (temp->next);
 }
