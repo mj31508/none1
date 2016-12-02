e7d96cd6b4f5e228513e77d5652ec27c0df81fe4
Max <mj31508@gmail.com>
2016-12-02 14:36:19 -0800

    10

---
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *delete_node_at_index - deleting a node
+ *@head: double pointer
+ *@index: index
+ *Return: Returns 1 if won, return -1 is lost
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *tempp;
+
+	unsigned int i;
+
+	if (*head == NULL)
+		return (-1);
+	i = 0;
+
+	tempp = *head;
+
+	if (i == index)
+	{
+		*head = (*head)->next;
+		free(tempp);
+		return (1);
+	}
+
+	while (tempp != NULL && i < index)
+	{
+		tempp = tempp->next;
+		i++;
+
+		if (tempp == NULL)
+			return (-1);
+	}
+	temp = tempp;
+	temp = temp->next;
+	tempp->next = temp->next;
+	free(temp);
+	return (1);
+}
