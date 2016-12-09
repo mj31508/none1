9c8a21a97628e9c5f44d73e98ce0f9daa9dffad0
Max <mj31508@gmail.com>
2016-12-08 20:43:23 -0800

    1

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ *create_file - creating a new file
+ *@text_content: character
+ *@filename: name of the file
+ *
+ *Return: something
+ **/
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fd, check, bers;
+	mode_t m;
+	m = S_IRUSR|S_IWUSR;
+	fd = open(filename, O_WRONLY|O_CREAT| O_TRUNC, m);
+	if (fd == -1)
+		return (-1);
+	if (text_content == '\0')
+	{
+		close(fd);
+		return (1);
+	}
+
+	bers = 0;
+	while (text_content[bers])
+		bers++;
+	if (bers > 0)
+		check = write(fd, text_content, bers);
+	close(fd);
+	if (check == -1)
+		return (-1);
+	return (1);
+}
