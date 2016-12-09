f12d52eb337ea917dba2c9c3067a73d218e02c78
Max <mj31508@gmail.com>
2016-12-08 20:51:07 -0800

    1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -12,8 +12,10 @@ int create_file(const char *filename, char *text_content)
 {
 	ssize_t fd, check, bers;
 	mode_t m;
-	m = S_IRUSR|S_IWUSR;
-	fd = open(filename, O_WRONLY|O_CREAT| O_TRUNC, m);
+
+	m = S_IRUSR | S_IWUSR;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, m);
 	if (fd == -1)
 		return (-1);
 	if (text_content == '\0')
