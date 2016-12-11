bad8c7c06c9eb803cafd6ea5cf5d912953a6999d
Max <mj31508@gmail.com>
2016-12-10 21:00:02 -0800

    5

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "hoberton.h"
+
+/**
+ * flip_bits - flip
+ *@n: 1st
+ *@m: 2nd
+ *Return: num of bits
+ **/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int much;
+	unsigned long int temp;
+
+	much = 0;
+	temp = n ^ m;
+	for (; temp > 0; temp >>=1)
+	{
+		if ((temp & 1) == 1)
+			much++;
+	}
+	return (much);
+}
